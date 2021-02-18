9b3d179c6c6ef32f73dc6280a4c2e9ea4eff3f7c
NatanLee <afkolya@mail.ru>
2021-02-18 23:10:59 +0300

    lesson 5 hw start

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ const API = 'https://fakestoreapi.com/';
 const app = new Vue({
     el: '#app',
     data: {
+        userSearch: '',
+        showCart: false,
+        cartUrl: 'cart',
         catalogUrl: 'products',
         products: [],
+        cartItems: [],
+        filtered: [],
         imgCatalog: 'https://placehold.it/200x150',
+        imgCart: 'https://placehold.it/50x100',    
     },
     methods: {
         getJson(url){
@@ -18,14 +24,26 @@ const app = new Vue({
         },
         addProduct(product){
             console.log(product.id);
-        }
+        },
+        filter(){           
+            const regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(product => regexp.test(product.title));
+            console.log('filter');
+        },
     },
     mounted(){
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
                 for(let el of data){
-                    this.products.push(el);
-                }
+                    this.filtered.push(el);
+                    this.products.push(el);                    
+                }                
             });
+        /* this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);                    
+                }                
+            }); */
     }
 });
